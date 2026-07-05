91e1f5aef22c3b2db64a6b7472091d5090c7c60c
Muhammad Faizan <faf2001f@gmail.com>
2026-07-06 02:52:05 +0500

    prepare for deployment

--- config/db.js
+++ config/db.js
@@ -1,16 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  if (!process.env.MONGO_URI) {
-    console.warn("MONGO_URI is not defined. Skipping database connection.");
-    return;
-  }
-
   try {
-    const conn = await mongoose.connect("mongodb+srv://jerutechadmin:J3ruT3ch%402026%21M0ng0DB%23Atlas@cluster0.v3fivoz.mongodb.net/JeruTech_Web?retryWrites=true&w=majority&appName=Cluster0");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
+    process.exit(1);
   }
 };
 
